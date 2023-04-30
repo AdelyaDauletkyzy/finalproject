a6cc382e7b117e208e1c8fc4c9920f9c1e4ad412
AdelyaDauletkyzy <122590270+AdelyaDauletkyzy@users.noreply.github.com>
2023-04-30 16:49:38 +0600

    Add files via upload

---
+++ adelyadauletkyzy_finalproject/mainjs(draft).js
@@ -0,0 +1,160 @@
+/*Menu*/
+const navMenu = document.getElementById("nav-menu"),
+      navToggle = document.getElementById("nav-toggle"),
+      navClose = document.getElementById("nav-close")
+
+
+if(navToggle){
+    navToggle.addEventListener("click", () =>{
+        navMenu.classList.add("show-menu")
+
+    })
+
+}
+
+if(navClose){
+    navClose.addEventListener("click", () =>{
+        navMenu.classList.remove("show-menu")
+
+    })
+
+}
+
+/*remove menu in mb*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*skills*/
+const skillsContent = document.getElementsByClassName('skills__content'),
+      skillsHeader =document.querySelectorAll('.skills__header')
+function toggleSkills(){
+    let itemClass = this.parentNode.className
+    
+    for(i=0; i<skillsContent.length; i++){
+    skillsContent[i].className = 'skills__content skills__close'}
+   
+    if(itemClass ==='skills__content skills__close'){
+        this.parentNode.className = 'skills__content skills__open'
+    }
+}
+skillsHeader.forEach((el)=>{
+    el.addEventListener('click', toggleSkills)
+})
+
+/*qualification*/
+const tabs = document.querySelectorAll('[data-target'),
+      tabContents = document.querySelectorAll('[data-content')
+tabs.forEach(tab =>{
+    tab.addEventListener('click', () =>{
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContents.forEach(tabContent =>{
+            tabContent.classList.remove('qualification__active')
+        })
+        target.classList.add('qualification__active')
+
+        tabs.forEach(tab =>{
+            tab.classList.remove('qualification__active')
+        })
+        tab.classList.add('qualification__active')
+    })
+})
+
+/*services*/
+
+const financeTitle = document.querySelector('.services__title');
+const financeModal = document.querySelector('.services__modal-services');
+
+financeTitle.addEventListener('click', () => {
+  financeModal.classList.toggle('active');
+});
+
+document.addEventListener('click', (event) => {
+  if (!event.target.closest('.services__item')) {
+    financeModal.classList.remove('active');
+  }
+});
+
+/*const modalViews = document.querySelectorAll('.services__modal'),
+      modalBtns = document.querySelectorAll('.services__button'),
+      modalCloses = document.querySelectorAll('.services__modal-close')
+
+let modal = function(modalClick){
+    modalViews[modalClick].classList.add('active-modal')
+}
+
+modalBtns.forEach((modalBtn, i) => {
+    modalBtn.addEventListener('click', () =>{
+        modal(i)
+    })
+})
+
+modalCloses.forEach((modalClose) =>{
+    modalClose.addEventListener('click', () =>{
+        modalViews.forEach((modalView) =>{
+            modalView.classList.remove('active-modal')
+        })
+    })
+})*/
+
+/*achivements*/
+let swiperPortfolio = new Swiper(".portfolio__container", {
+    cssMode: true,
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+/*extra*/
+let swiperTestimonial = new Swiper(".testimonial_container", {
+    loop: true,
+    grabCursor:true,
+    spaceBetween: 48,
+
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets:true,
+    },
+    breakpoints:{
+        568:{
+            slidesPerView:2,
+        }
+    }
+  });
+
+/*scroll*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+
+
+
